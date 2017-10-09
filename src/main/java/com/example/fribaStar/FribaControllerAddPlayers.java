caf692fc345782d436cf2051c7451f92fc4bdd2d
sepi <sepi@coyote>
2017-10-09 22:23:12 +0300

    sepin vajaa addplayer

---
+++ src/main/java/com/example/fribaStar/FribaControllerAddPlayers.java
@@ -0,0 +1,25 @@
+package com.example.fribaStar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FribaControllerAddPlayers {
+
+	@GetMapping("/addPlayers")
+	public String addPlayersForm(Model model)
+	{
+		model.addAttribute("player", new Player());
+		return "addPlayers";
+	}
+	
+	@PostMapping("addPlayers")
+	public String addPlayersSubmit(@ModelAttribute Player player)
+	{
+		return "addPlayers";
+	}
+	
+}
