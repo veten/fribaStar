7c165545001459c08f6998dc96e52eb21a2414bb
sepi <sepi@coyote>
2017-10-12 23:28:50 +0300

    jsonia frontista backiin ja takas

--- src/main/java/com/example/fribaStar/FribaControllerAddPlayers.java
+++ src/main/java/com/example/fribaStar/FribaControllerAddPlayers.java
@@ -1,10 +1,18 @@
 package com.example.fribaStar;
 
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FribaControllerAddPlayers {
@@ -16,11 +24,12 @@ public class FribaControllerAddPlayers {
 		return "addPlayers";
 	}
 	
-//	@PostMapping("/addPlayers")
-//	public String addPlayersSubmit(@ModelAttribute Player player)
-//	{
-//		System.out.println(player.toString());
-//		return "addPlayers";
-//	}
+	//@RequestMapping(path = "/addPlayers", produces = "application/json", method = RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(path = "/addPlayers", method = RequestMethod.POST)
+	public @ResponseBody String addPlayersSubmit(@RequestBody Map<String, Object> player) throws Exception
+	{	
+		System.out.println(player);
+		return "{\"Lisays\":\"toimii\"}" ;
+	}
 	
 }
