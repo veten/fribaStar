7080b0cf0e7677480f833f5d6f57a7f785158c72
gitsand <a.b@c.invalid>
2019-01-27 00:03:41 +0200

    Added CrossOrigin to allow UI separation to NodeJS

--- src/main/java/com/example/fribaStar/FribaControllerAddPlayers.java
+++ src/main/java/com/example/fribaStar/FribaControllerAddPlayers.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@CrossOrigin
 @Controller
 class FribaControllerAddPlayers {
 
--- src/main/java/com/example/fribaStar/FribaControllerResults.java
+++ src/main/java/com/example/fribaStar/FribaControllerResults.java
@@ -2,8 +2,10 @@ package com.example.fribaStar;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@CrossOrigin
 @Controller
 public class FribaControllerResults {
 		
